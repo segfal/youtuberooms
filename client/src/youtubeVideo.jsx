27614be306537b32250994176ecccc316537b62e
victornoel <vnoel890@gmail.com>
2023-07-16 18:55:31 -0400

    Add pause and resume function

--- client/src/youtubeVideo.jsx
+++ client/src/youtubeVideo.jsx
@@ -1,57 +1,48 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import ReactPlayer from "react-player";
 // import io from 'socket.io-client';
 // const socket = io('http://localhost:3001');
-const YoutubeVideo = ({ link , room, socket}) =>{ 
+const YoutubeVideo = ({ link, room, socket }) => {
   const [playing, setPlaying] = useState(true);
+  const test = "test";
   const pauseVideo = () => {
     console.log("pause");
-    socket.emit("on_pause", link);
-  }
+    socket.emit("on_pause", {room});
+  };
 
   const pauseAll = () => {
     setPlaying(false);
-  }
+  };
   const resumeVideo = () => {
     console.log("resume");
-    socket.emit("on_resume", room);
-    //console log current room
-
-    console.log(socket);
-
-  }
+    socket.emit("on_resume", {room});
+  };
 
   const resumeAll = () => {
-    setPlaying(true)
-  }
+    setPlaying(true);
+  };
   useEffect(() => {
-    socket.on("on_pause", () => {
-      alert("Hello");
-    })
-    // socket.on("pause", (data) => {
-    //   console.log("Hello")
-    // })
-   
-    // console.log("USE EFFECT FIRING")
-    // socket.on("pause", (data) => {
-    //   console.log("Pausing All")
-    //   pauseAll();
-    // })
+    socket.on("pause", () => {
+      pauseAll();
+    });
+    return () => {
+      socket.off("pause");
+    };
+  }, []);
 
-    // socket.on("resume", (data) => {
-    //   console.log("Resuming All")
-    //   resumeAll();
-    // })
+  useEffect(() => {
+    socket.on("resume", () => {
+      resumeAll();
+    });
+    return () => {
+      socket.off("resume");
+    };
+  }, []);
 
-
-  }, [])
-
-  
-  
-  return(
-  <div className="video-responsive">
-    {/* <iframe
+  return (
+    <div className="video-responsive">
+      {/* <iframe
       width="853"
       height="480"
       src={link}
@@ -61,31 +52,32 @@ const YoutubeVideo = ({ link , room, socket}) =>{
       title="Embedded youtube"
     /> */}
 
+      <ReactPlayer
+        url={`${link}?start=10`}
+        playing={playing}
+        controls={true}
+        width="853px"
+        height="480px"
+        onPause={() => console.log("paused")}
+        onPlay={() => console.log("playing")}
+        onEnded={() => {
+          "Hello World";
+        }}
+      />
 
-    <ReactPlayer url={link}
-    
-    playing={playing}
-    controls={true}
-    width="853px"
-    height="480px"
-    onPause={()=>console.log("paused")}
-    onPlay={()=>console.log("playing")}
-    onEnded={()=> {"Hello World"}}
-    />
-    
-    <button size="small">
-      {playing? (
-        <button onClick={pauseVideo}>Pause</button>
-      ) : (
-        <button onClick={resumeVideo}>Resume</button>
-      )}
-    </button>
-  </div>
-);
-}
+      <button size="small">
+        {playing ? (
+          <button onClick={pauseVideo}>Pause</button>
+        ) : (
+          <button onClick={resumeVideo}>Resume</button>
+        )}
+      </button>
+    </div>
+  );
+};
 
 YoutubeVideo.propTypes = {
-  link: PropTypes.string.isRequired
+  link: PropTypes.string.isRequired,
 };
 
 export default YoutubeVideo;
